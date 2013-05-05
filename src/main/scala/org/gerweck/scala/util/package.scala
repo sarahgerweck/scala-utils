881d74ef602d874cad5ca9de24135e0021f93d85
Sarah Gerweck <sarah.a180@gmail.com>
2013-05-04 18:24:26 -0700

    Better accuracy of `char =~= char`. Improved documentation.

--- src/main/scala/org/gerweck/scala/util/package.scala
+++ src/main/scala/org/gerweck/scala/util/package.scala
@@ -1,13 +1,39 @@
 package org.gerweck.scala
 
+/** Miscellaneous utility code for manipulating standard objects.
+  *  
+  * This package is designed to be maximally convenient if you `import org.gerweck.scala.util._`.
+  */ 
 package object util {
-  implicit final class CharUtils(val c: Char) extends AnyVal {
-    /** Case-insensitive equals */
-    @inline def =~= (d: Char): Boolean = c.toLower == d.toLower
+  implicit final class CharUtils(val self: Char) extends AnyVal {
+
+    /** Case-insensitive equals.
+      *  
+      * @note `~` is the command-mode vim keystroke to flip the case of a 
+      * character, and the defined operator looks similar to the mathematical 
+      * congruence operator (&cong;). These offer useful mnemonics.
+      */
+    @inline def =~= (that: Char): Boolean = {
+      /* 
+       * String's `equalsIgnoreCase` is safer than something like `toLower`. The 
+       * latter is not always safe when dealing with international characters. 
+       * (Passing in a specific locale is even better, but String provides good 
+       * generalized behavior.)
+       */
+      String.valueOf(self) =~= String.valueOf(that) 
+    }
   }
-  
-  implicit final class StringUtils(val s: String) extends AnyVal {
-    /** Case-insensitive equals */
-    @inline def =~= (t: String): Boolean = s equalsIgnoreCase t
+
+  /** Utility functionality for working with strings.   
+    * 
+    */
+  implicit final class StringUtils(val self: String) extends AnyVal {
+    /** Case-insensitive equals.
+      *  
+      * @note `~` is the command-mode vim keystroke to flip the case of a 
+      * character, and the defined operator looks similar to the mathematical 
+      * congruence operator (&cong;). These offer useful mnemonics.
+      */
+    @inline def =~= (that: String): Boolean = self equalsIgnoreCase that
   }
 }
