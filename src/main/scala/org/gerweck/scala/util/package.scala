a3b6c4feea4dbb0112f319ffeb0cdc876b968cd1
Sarah Gerweck <sarah.a180@gmail.com>
2013-05-04 18:58:09 -0700

    Improve documentation.

--- src/main/scala/org/gerweck/scala/util/package.scala
+++ src/main/scala/org/gerweck/scala/util/package.scala
@@ -3,8 +3,15 @@ package org.gerweck.scala
 /** Miscellaneous utility code for manipulating standard objects.
   *  
   * This package is designed to be maximally convenient if you `import org.gerweck.scala.util._`.
+  *
+  * @author Sarah Gerweck <sarah.a180@gmail.com>
   */ 
 package object util {
+  
+  /** Utility functionality for working with characters.   
+   * 
+   * @author Sarah Gerweck <sarah.a180@gmail.com>
+   */
   implicit final class CharUtils(val self: Char) extends AnyVal {
 
     /** Case-insensitive equals.
@@ -26,6 +33,7 @@ package object util {
 
   /** Utility functionality for working with strings.   
     * 
+    * @author Sarah Gerweck <sarah.a180@gmail.com>
     */
   implicit final class StringUtils(val self: String) extends AnyVal {
     /** Case-insensitive equals.
@@ -34,6 +42,8 @@ package object util {
       * character, and the defined operator looks similar to the mathematical 
       * congruence operator (&cong;). These offer useful mnemonics.
       */
-    @inline def =~= (that: String): Boolean = self equalsIgnoreCase that
+    @inline def =~= (that: String): Boolean = 
+      if (self eq that) true 
+      else self equalsIgnoreCase that
   }
 }
