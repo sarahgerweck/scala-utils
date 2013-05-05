0d89b7e7f7cf380b1a547cc42f44a9226b2d3ffd
Sarah Gerweck <sarah.a180@gmail.com>
2013-05-04 20:38:00 -0700

    Improve formatting & performance.

--- src/main/scala/org/gerweck/scala/util/timed.scala
+++ src/main/scala/org/gerweck/scala/util/timed.scala
@@ -19,19 +19,23 @@ object timed {
       throw e
     } finally {
       val finishTime = nanoTime
-      logger(level)(s"${taskName.capitalize} ${if (failed) "failed" else "completed"} after ${formatDuration((finishTime - startTime) * 1e-9f)}")
+      logger(level)(s"${taskName.capitalize} ${if (failed) "failed" else "completed"} after ${formatDuration(1e-9f * (finishTime - startTime))}")
     }
   }
   
   def formatDuration(t: Float): String = {
     require(t >= 0f, "Cannot format a negative duration")
-    if      (t == 0)    "0 s"
+    if      (t == 0f)    "0 s"
     else if (t < 1e-6f) "< 1 μs"
     else if (t < 1e-3f) f"${1e6f * t}%.3g μs"
     else if (t < 1f)    f"${1e3f * t}%.3g ms"
     else if (t < 1e3f)  f"$t%.3g s"
+    else if (t < 1e6f)  f"${1e-3f * t}%.3g ks"
     else                f"$t%.4g s"
   }
   
-  def formatSince(startTime: Long): String = formatDuration((nanoTime - startTime) * 1e-9f)
+  @inline def formatSince(startTime: Long): String = {
+    val finishTime = nanoTime
+    formatDuration(1e-9f * (finishTime - startTime))
+  }
 }
