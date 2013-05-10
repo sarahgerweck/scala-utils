9fc669feb0f8a2a3811d7466de88f3f3a4e83551
Sarah Gerweck <sarah.a180@gmail.com>
2013-05-10 08:25:47 -0700

    Add type-based element parser.

---
+++ src/main/scala/org/gerweck/scala/util/ParserUtil.scala
@@ -0,0 +1,21 @@
+package org.gerweck.scala.util
+
+import scala.reflect.{ ClassTag, classTag }
+import scala.util.parsing.combinator.syntactical.TokenParsers
+
+
+/** Mix-in utility code to assist with Parser development.
+  * 
+  * @author Sarah Gerweck <sarah.a180@gmail.com>
+  */
+trait ParserUtil extends TokenParsers {
+  
+  /** A parser that matches elements of a given type.  This is useful if you have 
+    * a hierarchy of traits that extend from `Elem` and want to easily be able to
+    * match certain classes of tokens.
+    */
+  @inline final protected[this] def tpe[A <: Elem : ClassTag]: Parser[A] = { 
+    val clzz = classTag[A].runtimeClass
+    elem(clzz.getSimpleName, { clzz isInstance _ }).asInstanceOf[Parser[A]]
+  }
+}
