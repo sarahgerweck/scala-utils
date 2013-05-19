b95dc78d331365cd855d35881027556b156f9130
Sarah Gerweck <sarah.a180@gmail.com>
2013-05-19 11:37:40 -0700

    Rename ParserUtil to TokenParserUtil

--- src/main/scala/org/gerweck/scala/util/TokenParserUtil.scala
+++ src/main/scala/org/gerweck/scala/util/TokenParserUtil.scala
@@ -8,13 +8,13 @@ import scala.util.parsing.combinator.syntactical.TokenParsers
   * 
   * @author Sarah Gerweck <sarah.a180@gmail.com>
   */
-trait ParserUtil extends TokenParsers {
+trait TokenParserUtil extends TokenParsers {
   
   /** A parser that matches elements of a given type.  This is useful if you have 
     * a hierarchy of traits that extend from `Elem` and want to easily be able to
     * match certain classes of tokens.
     */
-  @inline final protected[this] def tpe[A <: Elem : ClassTag]: Parser[A] = { 
+  final protected[this] def tpe[A <: Elem : ClassTag]: Parser[A] = { 
     val clzz = classTag[A].runtimeClass
     elem(clzz.getSimpleName, { clzz isInstance _ }).asInstanceOf[Parser[A]]
   }
