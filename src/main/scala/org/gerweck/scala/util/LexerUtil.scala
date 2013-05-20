4482ae181f54802fa23fd81fb46ff4ccaf1fde23
Sarah Gerweck <sarah.a180@gmail.com>
2013-05-19 23:10:02 -0700

    Memoize string parsers.
    Previously, we would create a new parser every time it was called, which
    can lead to performance problems in some situations.

--- src/main/scala/org/gerweck/scala/util/LexerUtil.scala
+++ src/main/scala/org/gerweck/scala/util/LexerUtil.scala
@@ -2,7 +2,10 @@ package org.gerweck.scala.util
 
 import language.implicitConversions
 
+import java.util.concurrent.ConcurrentHashMap
+
 import scala.annotation.tailrec
+import scala.collection.JavaConversions._
 
 // Parsing imports
 import scala.util.parsing._
@@ -29,9 +32,12 @@ trait LexerUtil extends Lexical with ParserUtil {
         
     lex(new Scanner(input), Vector.empty)
   }
+
+  private[this] val charParsers = mapAsScalaConcurrentMap(new ConcurrentHashMap[(String,Boolean),Parser[Vector[Char]]])
+  private[this] val strParsers = mapAsScalaConcurrentMap(new ConcurrentHashMap[(String,Boolean),Parser[String]])
   
   // TBD: This might perform better as a macro 
-  final def chars(s: String, sensitive: Boolean = false): Parser[Vector[Char]] = 
+  final def chars(s: String, sensitive: Boolean = false): Parser[Vector[Char]] = charParsers.getOrElseUpdate((s, sensitive), {
     (s :\ success(Vector.empty[Char])) { (head, tailParser) =>
       @inline def headParser = 
         if (sensitive) elem(head)
@@ -39,8 +45,11 @@ trait LexerUtil extends Lexical with ParserUtil {
         
       headParser ~ tailParser ^^ { case h ~ t => h +: t } 
     }
+  })
   
   // TBD: This might perform better as a macro 
-  final def str(s: String, sensitive: Boolean = false): Parser[String] = chars(s, sensitive) ^^ { _.mkString }
+  final def str(s: String, sensitive: Boolean = false): Parser[String] = strParsers.getOrElseUpdate((s, sensitive), {
+    chars(s, sensitive) ^^ { _.mkString }
+  })
      
 }
