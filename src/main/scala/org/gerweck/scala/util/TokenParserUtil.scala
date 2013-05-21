ddaf651bc60463ab36f761c374fe9386ecc99583
Sarah Gerweck <sarah.a180@gmail.com>
2013-05-20 22:28:12 -0700

    Memoize any type-based parsers we create.
    They are pretty simple, but they have a tendency to get created in a lot
    of different places so let's stick them in a little map just in case.
    There can never be *too* many of them, since they have to be based on an
    actual type.

--- src/main/scala/org/gerweck/scala/util/TokenParserUtil.scala
+++ src/main/scala/org/gerweck/scala/util/TokenParserUtil.scala
@@ -1,5 +1,8 @@
 package org.gerweck.scala.util
 
+import java.util.concurrent.ConcurrentHashMap
+
+import scala.collection.JavaConversions._
 import scala.reflect.{ ClassTag, classTag }
 import scala.util.parsing.combinator.syntactical.TokenParsers
 
@@ -9,13 +12,19 @@ import scala.util.parsing.combinator.syntactical.TokenParsers
   * @author Sarah Gerweck <sarah.a180@gmail.com>
   */
 trait TokenParserUtil extends TokenParsers with ParserUtil {
+
+  private[this] val typeParsers = new ConcurrentHashMap[ClassTag[_ <: Elem], Parser[Elem]]
   
   /** A parser that matches elements of a given type.  This is useful if you have 
     * a hierarchy of traits that extend from `Elem` and want to easily be able to
     * match certain classes of tokens.
     */
   final protected[this] def tpe[A <: Elem : ClassTag]: Parser[A] = { 
-    val clzz = classTag[A].runtimeClass
-    elem(clzz.getSimpleName, { clzz isInstance _ }).asInstanceOf[Parser[A]]
+    val ct = classTag[A]
+    val p = typeParsers getOrElseUpdate (ct, {
+      val clzz = ct.runtimeClass
+      elem(clzz.getSimpleName, { clzz isInstance _ })
+    })
+    p.asInstanceOf[Parser[A]]
   }
 }
