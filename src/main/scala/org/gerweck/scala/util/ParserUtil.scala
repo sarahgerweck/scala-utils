62cd9ca505ebd638cee9cfaa8ee4fe20ad753f15
Sarah Gerweck <sarah.a180@gmail.com>
2013-09-26 02:57:26 -0700

    Generalize the separated-repeat parsers.

--- src/main/scala/org/gerweck/scala/util/ParserUtil.scala
+++ src/main/scala/org/gerweck/scala/util/ParserUtil.scala
@@ -4,17 +4,29 @@ import scala.util.parsing.combinator.Parsers
 
 trait ParserUtil extends Parsers {
 
-  def cntsep[T](p: Int => Parser[T], q: => Parser[Any]): Parser[Seq[T]] = {
-    cnt1sep(p, q) | success(Seq.empty[T])
-  }
-  
-  def cnt1sep[T](p: Int => Parser[T], q: => Parser[Any]): Parser[Seq[T]] = {
+  def cntsep[T](p: Int => Parser[T], q: => Parser[Any]): Parser[Seq[T]] =
+    smartsep( { case n => (false, p(n)) }, q)
+
+  def cnt1sep[T](p: Int => Parser[T], q: => Parser[Any]): Parser[Seq[T]] =
+    smartsep( { case 0 => (true, p(0))
+                case n => (false, p(n))
+              }
+            , q)
+
+  def smartsep[T](p: PartialFunction[Int,(Boolean,Parser[T])], q: => Parser[Any]): Parser[Seq[T]] = {
     lazy val q0 = q
-    def tail(count: Int): Parser[Seq[T]] =
-      ( q0 ~> p(count) ~ tail(count + 1) ^^ { case a ~ b => a +: b }
-      | success(List.empty[T])
-      )
-    
-    p(0) ~ tail(1) ^^ { case a ~ b => a +: b }
+    def tail(count: Int): Parser[Seq[T]] = {
+      if (p isDefinedAt count) {
+        val (required, parser) = p(count)
+        ( (  (if (count > 0) q0 else success(()))
+          ~> parser ~ tail(count + 1)
+          ^^ { case a ~ b => a +: b }
+          )
+        | success(List.empty[T])
+        )
+      } else success(List.empty[T])
+    }
+
+    tail(0)
   }
 }
