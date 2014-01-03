4ccf967ec4d45775f2bb654d97ac8b028c78e0b4
Sarah Gerweck <sarah.a180@gmail.com>
2014-01-02 22:52:10 -0800

    Remove some inline annotations.
    Scala has become more conservative about inlining, so don't force it.

--- src/main/scala/org/gerweck/scala/util/package.scala
+++ src/main/scala/org/gerweck/scala/util/package.scala
@@ -1,60 +1,60 @@
 package org.gerweck.scala
 
 /** Miscellaneous utility code for manipulating standard objects.
-  *  
+  *
   * This package is designed to be maximally convenient if you `import org.gerweck.scala.util._`.
   *
   * @author Sarah Gerweck <sarah.a180@gmail.com>
-  */ 
+  */
 package object util {
-  
-  /** Utility functionality for working with characters.   
-   * 
+
+  /** Utility functionality for working with characters.
+   *
    * @author Sarah Gerweck <sarah.a180@gmail.com>
    */
   implicit final class CharUtils(val self: Char) extends AnyVal {
-    
+
     /** Convert a character to a random case.  It will have a 50% probability of
       * being uppercase and a 50% probability of being lowercase.
       */
     def toRandomCase: Char = if (scala.util.Random.nextBoolean) self.toUpper else self.toLower
 
     /** Case-insensitive equals.
-      *  
-      * @note `~` is the command-mode vim keystroke to flip the case of a 
-      * character, and the defined operator looks similar to the mathematical 
+      *
+      * @note `~` is the command-mode vim keystroke to flip the case of a
+      * character, and the defined operator looks similar to the mathematical
       * congruence operator (&cong;). These offer useful mnemonics.
       */
-    @inline def =~= (that: Char): Boolean = {
-      /* 
-       * String's `equalsIgnoreCase` is safer than something like `toLower`. The 
-       * latter is not always safe when dealing with international characters. 
-       * (Passing in a specific locale is even better, but String provides good 
+    def =~= (that: Char): Boolean = {
+      /*
+       * String's `equalsIgnoreCase` is safer than something like `toLower`. The
+       * latter is not always safe when dealing with international characters.
+       * (Passing in a specific locale is even better, but String provides good
        * generalized behavior.)
        */
-      String.valueOf(self) =~= String.valueOf(that) 
+      String.valueOf(self) =~= String.valueOf(that)
     }
   }
 
-  /** Utility functionality for working with strings.   
-    * 
+  /** Utility functionality for working with strings.
+    *
     * @author Sarah Gerweck <sarah.a180@gmail.com>
     */
   implicit final class StringUtils(val self: String) extends AnyVal {
-    
+
     /** Convert a string to random case.  Each character will have a 50%
-      * probability of being uppercase and a 50% probability of being lowercase. 
+      * probability of being uppercase and a 50% probability of being lowercase.
       */
     def toRandomCase: String = self map { _.toRandomCase }
-    
+
     /** Case-insensitive equals.
-      *  
-      * @note `~` is the command-mode vim keystroke to flip the case of a 
-      * character, and the defined operator looks similar to the mathematical 
+      *
+      * @note `~` is the command-mode vim keystroke to flip the case of a
+      * character, and the defined operator looks similar to the mathematical
       * congruence operator (&cong;). These offer useful mnemonics.
       */
-    @inline def =~= (that: String): Boolean = 
-      if (self eq that) true 
+    def =~= (that: String): Boolean =
+      if (self eq that) true
       else self equalsIgnoreCase that
   }
 
