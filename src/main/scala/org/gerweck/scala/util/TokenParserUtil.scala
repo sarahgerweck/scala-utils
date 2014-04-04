ca2624dae85930ad472d6709076753ef73220053
Sarah Gerweck <sarah.a180@gmail.com>
2014-04-04 16:28:58 -0700

    Small cleanup of formatting.

--- src/main/scala/org/gerweck/scala/util/TokenParserUtil.scala
+++ src/main/scala/org/gerweck/scala/util/TokenParserUtil.scala
@@ -8,18 +8,18 @@ import scala.util.parsing.combinator.syntactical.TokenParsers
 
 
 /** Mix-in utility code to assist with Parser development.
-  * 
+  *
   * @author Sarah Gerweck <sarah.a180@gmail.com>
   */
 trait TokenParserUtil extends TokenParsers with ParserUtil {
 
   private[this] val typeParsers = new ConcurrentHashMap[ClassTag[_ <: Elem], Parser[Elem]]
-  
-  /** A parser that matches elements of a given type.  This is useful if you have 
+
+  /** A parser that matches elements of a given type.  This is useful if you have
     * a hierarchy of traits that extend from `Elem` and want to easily be able to
     * match certain classes of tokens.
     */
-  final protected[this] def tpe[A <: Elem : ClassTag]: Parser[A] = { 
+  final protected[this] def tpe[A <: Elem : ClassTag]: Parser[A] = {
     val ct = classTag[A]
     val p = typeParsers getOrElseUpdate (ct, {
       val clzz = ct.runtimeClass
