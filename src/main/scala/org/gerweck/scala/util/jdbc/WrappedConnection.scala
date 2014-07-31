fc6960c27c10f2865ef51ed284aa1c0acc79f652
Sarah Gerweck <sarah.a180@gmail.com>
2014-07-30 18:08:19 -0700

    Improved variable naming.

--- src/main/scala/org/gerweck/scala/util/jdbc/WrappedConnection.scala
+++ src/main/scala/org/gerweck/scala/util/jdbc/WrappedConnection.scala
@@ -69,8 +69,8 @@ class WrappedConnection(val inner: Connection) extends Connection {
   def unwrap[T](x$1: Class[T]): T = { inner.unwrap(x$1) }
 
   override def hashCode: Int = inner.hashCode
-  override def equals(o: Any) = o match {
+  override def equals(that: Any) = that match {
     case wc: WrappedConnection => inner equals wc.inner
-    case _                     => inner equals o
+    case _                     => inner equals that
   }
 }
