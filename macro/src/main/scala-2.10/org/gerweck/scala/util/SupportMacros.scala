5900eca224b5357f2c5f33c39fd6d8d64420e5e8
Sarah Gerweck <sarah.a180@gmail.com>
2014-08-29 22:33:12 -0700

    Update support to use new macro layout.

---
+++ macro/src/main/scala-2.10/org/gerweck/scala/util/SupportMacros.scala
@@ -0,0 +1,56 @@
+package org.gerweck.scala.util
+
+import language.experimental.macros
+
+import scala.reflect.macros.Context
+
+import org.log4s._
+
+private object SupportMacros {
+  def support_simple(c: Context): c.Expr[Nothing] = {
+    import c.universe._
+
+    val callerName = getCallerName(c)
+
+    val message = s"Method `$callerName` not supported"
+
+    reify {
+      throw new java.lang.UnsupportedOperationException(c.literal(message).splice)
+    }
+  }
+
+  def support_complex(c: Context)(logger: c.Expr[Logger], level: c.Expr[LogLevel]): c.Expr[Nothing] = {
+    import c.universe._
+
+    val callerName = getCallerName(c)
+
+    val exceptionMessage = s"Method `$callerName` not supported"
+    val logMessage = s"Got unsupported call to `$callerName`"
+
+    reify {
+      (logger.splice).apply(level.splice).apply(c.literal(logMessage).splice)
+      throw new java.lang.UnsupportedOperationException(c.literal(exceptionMessage).splice)
+    }
+  }
+
+  // TODO: Reduce code duplication.
+  def support_warn(c: Context): c.Expr[Nothing] = {
+    import c.universe._
+
+    val logger = c.Expr[Logger](Ident(newTermName("logger")))
+
+    val callerName = getCallerName(c)
+    val logMessage = s"Got unsupported call to `$callerName`"
+    val exceptionMessage = s"Method `$callerName` is not supported"
+
+    reify {
+      (logger.splice).warn(c.literal(logMessage).splice)
+      throw new java.lang.UnsupportedOperationException(c.literal(exceptionMessage).splice)
+    }
+  }
+
+  @inline private def getCallerName(c: Context): String = {
+    val caller = c.enclosingMethod
+    caller.symbol.name.decodedName.toString
+  }
+}
---
+++ macro/src/main/scala-2.11/org/gerweck/scala/util/SupportMacros.scala
@@ -0,0 +1,48 @@
+package org.gerweck.scala.util
+
+import language.experimental.macros
+
+import scala.reflect.macros.whitebox.Context
+
+import org.log4s._
+
+private object SupportMacros {
+  def support_simple(c: Context): c.Expr[Nothing] = {
+    import c.universe._
+
+    val callerName = getCallerName(c)
+
+    val message = s"Method `$callerName` not supported"
+
+    c.Expr[Nothing](q"throw new java.lang.UnsupportedOperationException($message)")
+  }
+
+  def support_complex(c: Context)(logger: c.Expr[Logger], level: c.Expr[LogLevel]): c.Expr[Nothing] = {
+    import c.universe._
+
+    val callerName = getCallerName(c)
+
+    val exceptionMessage = s"Method `$callerName` not supported"
+    val logMessage = s"Got unsupported call to `$callerName`"
+
+    c.Expr[Nothing](q"$logger.apply($level).apply($logMessage); throw new java.lang.UnsupportedOperationException($exceptionMessage)")
+  }
+
+  // TODO: Reduce code duplication.
+  def support_warn(c: Context): c.Expr[Nothing] = {
+    import c.universe._
+
+    val logger = c.Expr[Logger](Ident(TermName("logger")))
+
+    val callerName = getCallerName(c)
+    val logMessage = s"Got unsupported call to `$callerName`"
+    val exceptionMessage = s"Method `$callerName` is not supported"
+
+    c.Expr[Nothing](q"$logger.warn($logMessage); throw new java.lang.UnsupportedOperationException($exceptionMessage)")
+  }
+
+  @inline private def getCallerName(c: Context): String = {
+    val caller = c.enclosingMethod
+    caller.symbol.name.decodedName.toString
+  }
+}
