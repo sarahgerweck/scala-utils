aeb26c056d0ddeb02d4cd5eaa10f33c1c5de3247
Sarah Gerweck <sarah.a180@gmail.com>
2014-09-24 00:57:49 -0700

    Add integer square root functions to math.

--- src/main/scala/org/gerweck/scala/util/math/package.scala
+++ src/main/scala/org/gerweck/scala/util/math/package.scala
@@ -11,4 +11,52 @@ package object math {
   implicit final class HexByte(val sc: StringContext) extends AnyVal {
     def b16(): Byte = macro HexByteMacros.hexByte
   }
+
+  def isqrt(i: Int): Int = {
+    if (i < 0) throw new IllegalArgumentException("No square roots for negative numbers")
+
+    var one = 1 << 30
+    var op = i
+    var res = 0
+    while (one > op) {
+      one >>>= 2
+    }
+    while (one != 0) {
+      assert (op >= 0)
+      /* If res + one is negative, we had a signed overflow and it's actually greater than op.
+       * Every operation we do other than the comparison is effectively unsigned.
+       */
+      if (op >= res + one || res + one < 0) {
+        op -= res + one
+        res += 2 * one
+      }
+      res >>>= 1
+      one >>>= 2
+    }
+    res
+  }
+
+  def isqrt(l: Long): Long = {
+    if (l < 0) throw new IllegalArgumentException("No square roots for negative numbers")
+
+    var one = 1L << 62
+    var op = l
+    var res = 0L
+    while (one > op) {
+      one >>>= 2
+    }
+    while (one != 0) {
+      assert (op >= 0)
+      /* If res + one is negative, we had a signed overflow and it's actually greater than op.
+       * Every operation we do other than the comparison is effectively unsigned.
+       */
+      if (op >= res + one || res + one < 0) {
+        op -= res + one
+        res += 2 * one
+      }
+      res >>>= 1
+      one >>>= 2
+    }
+    res
+  }
 }
--- src/test/scala/org/gerweck/scala/util/MathSpec.scala
+++ src/test/scala/org/gerweck/scala/util/MathSpec.scala
@@ -1,12 +1,13 @@
 package org.gerweck.scala.util.math
 
 import org.scalatest._
+import org.scalatest.prop.PropertyChecks
 
 /** Test specs for math utility functions
   *
   * @author Sarah Gerweck <sarah.a180@gmail.com>
   */
-class MathSpec extends FlatSpec with Matchers {
+class MathSpec extends FlatSpec with Matchers with PropertyChecks {
   behavior of "Hexadecimal byte literals"
 
   it should "generate bytes" in {
@@ -26,5 +27,60 @@ class MathSpec extends FlatSpec with Matchers {
     b16"09".toInt should (be >= 0)
     b16"a0".toInt should (be < 0)
     b16"ff".toInt should (be < 0)
+  }
+
+  behavior of "Integer square roots"
+
+  it should "handle a few common cases" in {
+    @inline def se(n: Long, r: Long) = {
+      isqrt(n) should equal (r)
+      isqrt(n.toInt) should equal (r)
+    }
+    se(0, 0)
+    se(1, 1)
+    se(2, 1)
+    se(3, 1)
+    se(4, 2)
+    se(5, 2)
+    se(8, 2)
+    se(9, 3)
+    se(16, 4)
+    se(25, 5)
+    se(36, 6)
+    se(49, 7)
+    se(64, 8)
+    se(256, 16)
+  }
+
+  it should "reject negative Ints" in {
+    forAll { i: Int =>
+      whenever (i < 0) {
+        an [IllegalArgumentException] should be thrownBy isqrt(i)
+      }
+    }
+  }
+
+  it should "reject negative Longs" in {
+    forAll { l: Long =>
+      whenever (l < 0) {
+        an [IllegalArgumentException] should be thrownBy isqrt(l)
+      }
+    }
+  }
+
+  it should "handle any Int's square root" in {
+    forAll { i: Int =>
+      whenever (i >= 0) {
+        isqrt(i) should equal (scala.math.sqrt(i).toInt)
+      }
+    }
+  }
+
+  it should "handle any Long's square root" in {
+    forAll { l: Long =>
+      whenever (l >= 0) {
+        isqrt(l) should equal (scala.math.sqrt(l).toLong)
+      }
+    }
   }
 }
