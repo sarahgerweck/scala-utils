40e5619fbf06f4d9520ccf0882b15493eedc5731
Sarah Gerweck <sarah.a180@gmail.com>
2014-11-11 01:32:28 -0800

    Updates for 1.0.1 releases.

--- src/main/scala/org/gerweck/scala/util/date/package.scala
+++ src/main/scala/org/gerweck/scala/util/date/package.scala
@@ -2,6 +2,8 @@ package org.gerweck.scala.util
 
 import org.joda.time._
 
+import scala.concurrent.duration.FiniteDuration
+
 package object date {
   implicit final class RichDate(val inner: LocalDateTime) extends AnyVal {
     def getQuarter = (inner.getMonthOfYear + 2) / 3
@@ -13,18 +15,22 @@ package object date {
 
     def + (p: ReadablePeriod) = inner plus p
     def + (d: ReadableDuration) = inner plus d
+    def + (d: FiniteDuration) = inner plus Duration.millis(d.toMillis)
 
     def - (p: ReadablePeriod) = inner minus p
     def - (d: ReadableDuration) = inner minus d
+    def - (d: FiniteDuration) = inner minus Duration.millis(d.toMillis)
   }
 
   implicit final class RichInstant(val inner: Instant) extends AnyVal {
     def - (other: Instant): Duration = new Duration(inner.getMillis - other.getMillis)
     def - (duration: Long): Instant = inner minus duration
     def - (duration: ReadableDuration): Instant = inner minus duration
+    def - (d: FiniteDuration) = inner minus Duration.millis(d.toMillis)
 
     def + (duration: Long): Instant = inner plus duration
     def + (duration: ReadableDuration): Instant = inner plus duration
+    def + (d: FiniteDuration) = inner plus Duration.millis(d.toMillis)
 
     def <  (that: Instant) = (inner compareTo that) <  0
     def <= (that: Instant) = (inner compareTo that) <= 0
