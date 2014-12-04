f60c9c0640c02bf41400e51b6972abbde938affe
Sarah Gerweck <sarah.a180@gmail.com>
2014-12-03 19:14:33 -0800

    Add new "using" methods for async callers.

--- src/main/scala/org/gerweck/scala/util/resource.scala
+++ src/main/scala/org/gerweck/scala/util/resource.scala
@@ -2,6 +2,8 @@ package org.gerweck.scala.util
 
 import language.reflectiveCalls
 
+import scala.concurrent._
+
 object resource {
   // TODO: Make this a macro
   final def using[A <: AutoCloseable,B](ac: A)(body: A => B): B =
@@ -11,4 +13,16 @@ object resource {
   final def usingDynamic[A <: { def close(): Unit },B](rsrc: A)(body: A => B): B =
     try { body(rsrc) }
     finally { rsrc.close() }
+
+  final def usingAsync[A <: AutoCloseable, B](ac: A)(body: A => Future[B])(implicit ec: ExecutionContext): Future[B] = {
+    val resultFuture = body(ac)
+    resultFuture onComplete { _ => ac.close() }
+    resultFuture
+  }
+
+  final def usingAsyncDynamic[A <: { def close(): Unit }, B](rsrc: A)(body: A => Future[B])(implicit ec: ExecutionContext): Future[B] = {
+    val resultFuture = body(rsrc)
+    resultFuture onComplete { _ => rsrc.close() }
+    resultFuture
+  }
 }
