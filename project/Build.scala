0eca21d4a402fefb05e7e9b8e3e555bb355bbf1e
Sarah Gerweck <sarah.a180@gmail.com>
2015-03-05 15:27:45 -0800

    Bump to Scala 2.11.6 and 2.10.5.

--- project/Build.scala
+++ project/Build.scala
@@ -12,8 +12,8 @@ import Helpers._
 sealed trait Basics {
   final val buildOrganization  = "org.gerweck.scala"
 
-  final val buildScalaVersion  = "2.11.5"
-  final val extraScalaVersions = Seq("2.10.4")
+  final val buildScalaVersion  = "2.11.6"
+  final val extraScalaVersions = Seq("2.10.5")
   final val buildJavaVersion   = "1.6"
   lazy  val defaultOptimize    = true
 
