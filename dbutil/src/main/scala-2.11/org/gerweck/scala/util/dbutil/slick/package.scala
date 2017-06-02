b658ce708bf78c9ba4a58e39173bf960ae024bdc
Sarah Gerweck <sarah.a180@gmail.com>
2017-06-01 17:53:13 -0700

    No longer use different versions of Slick

--- dbutil/src/main/scala-2.11/org/gerweck/scala/util/dbutil/slick/package.scala
+++
@@ -1,9 +0,0 @@
-package org.gerweck.scala.util.dbutil
-
-/**
-  *
-  * @author Sarah Gerweck <sarah@atscale.com>
-  */
-package object slick {
-  private[slick] type SProfile = _root_.slick.driver.JdbcProfile
-}
