8c5753c23f7696a9b57ff46d015ac59fe2ce93d1
Sarah Gerweck <sarah.a180@gmail.com>
2017-11-02 02:39:12 -0700

    Update to Scala 2.12.4

--- project/ProjectSettings.scala
+++ project/ProjectSettings.scala
@@ -11,7 +11,7 @@ trait ProjectSettings
   override final val githubOrganization    = "sarahgerweck"
   override final val githubProject         = "scala-utils"
 
-  override final val buildScalaVersion     = "2.12.3"
+  override final val buildScalaVersion     = "2.12.4"
   override final val extraScalaVersions    = Seq("2.11.11")
   override final val minimumJavaVersion    = "1.6"
   override final val defaultOptimize       = true
