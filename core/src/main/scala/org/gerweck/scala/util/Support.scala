4809eeaeb28897ccaeffe9654ef056600301b678
Sarah Gerweck <sarah.a180@gmail.com>
2019-07-31 23:50:00 -0700

    Add explicit return types for macros

--- core/src/main/scala/org/gerweck/scala/util/Support.scala
+++ core/src/main/scala/org/gerweck/scala/util/Support.scala
@@ -7,9 +7,9 @@ import org.log4s._
 object Support {
   import SupportMacros._
 
-  def warnSupport = macro support_warn
+  def warnSupport: Nothing = macro support_warn
 
   def support: Nothing = macro support_simple
 
-  def support(logger: Logger, level: LogLevel) = macro support_complex
+  def support(logger: Logger, level: LogLevel): Nothing = macro support_complex
 }
